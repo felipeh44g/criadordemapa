76c6f4c4492d9f8b22fede6e6c8b60b6f8832282
Pichau <Pichau@192.168.2.2>
2018-07-27 00:13:39 -0300

    new commit

---
+++ src/recurso/Recurso.java
@@ -0,0 +1,15 @@
+package recurso;
+
+import java.awt.Image;
+
+public class Recurso {
+
+    Image img;
+    boolean andavel;
+    
+    public Recurso(Image img, boolean andavel) {
+        this.img = img;
+        this.andavel = andavel;        
+    }
+    
+}
